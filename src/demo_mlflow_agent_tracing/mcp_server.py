fda235ec90e0090136964bbf1739496d731d3111
Taylor Agarwal <taagarwa@redhat.com>
2026-01-29 08:32:50 -0500

    ⚡️ Add logs and k argument to search tool

--- src/demo_mlflow_agent_tracing/mcp_server.py
+++ src/demo_mlflow_agent_tracing/mcp_server.py
@@ -1,28 +1,36 @@
+import logging
+
 from fastmcp import FastMCP
 
 from demo_mlflow_agent_tracing.db import get_db
+
+logger = logging.getLogger(__name__)
 
 mcp = FastMCP("Knowledge Base")
 
 
 @mcp.tool
-def search(query: str):
+def search(query: str, k: int = 3):
     """
     Search the knowledge base using semantic search.
 
     Args:
         query (str): A natural language query to search the knowledge base.
+        k (int, optional): Number of search results to return. Defaults to 3.
 
     """
+    logger.info(f"Search requested. {query=}")
     try:
         db = get_db()
-        documents = db.search(query=query, search_type="similarity")
+        documents = db.similarity_search(query=query, k=k)
+        logger.info(f"Found {len(documents)} results")
         return {
             "result": "success",
             "documents": documents,
         }
 
     except Exception as e:
+        logger.error(e, exc_info=True)
         return {
             "result": "error",
             "message": f"Search failed with error: {str(e)}",
@@ -30,4 +38,5 @@ def search(query: str):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
     mcp.run(show_banner=False)
